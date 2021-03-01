492ea7964136b293c569224235126dfed8514217
Test-Unity-Game <djcmanuel77@gmail.com>
2021-03-01 12:49:20 -0600

    Error fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -27,7 +27,7 @@ public class AutonomousOpMode extends LinearOpMode {
     static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0;     // For figuring circumference
     static final double     WHEEL_DIAMETER_FEET     = WHEEL_DIAMETER_INCHES/12.0;
-    static final double     COUNTS_PER_FOOT         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_FEET * 3.1415)
+    static final double     COUNTS_PER_FOOT         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_FEET * 3.1415);
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);                   // counts per revolution over circumference
     static final double     DRIVE_SPEED             = 0.6;
